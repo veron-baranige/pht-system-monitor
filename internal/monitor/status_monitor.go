9f2d7fd65878671dc3d421cb894bd5c6e9f39f5d
Veron Baranige <veron.b@hsenidoutsourcing.com>
2024-06-08 12:43:57 +0530

    Enhance health status error handling

--- internal/monitor/status_monitor.go
+++ internal/monitor/status_monitor.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 )
 
@@ -24,32 +25,35 @@ const (
 func GetHealthStatus(ctx context.Context, appBaseUrl string) (HealthStatus, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, appBaseUrl+healthEndpoint, nil)
 	if err != nil {
-		return "", fmt.Errorf("client: could not create request: %s", err)
+		return Unknown, fmt.Errorf("http client could not create request: %s", err)
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return Unknown, fmt.Errorf("client: error making request: %v", err)
+		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+			return "", ErrNotResponding
+		}
+		return Unknown, fmt.Errorf("http client error making request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return Unknown, fmt.Errorf("client: error reading response body: %v", err)
+		return Unknown, fmt.Errorf("http client error reading response body: %v", err)
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return Unknown, fmt.Errorf("client: received non-200 response: %s", body)
+		return Unknown, fmt.Errorf("http client received non-200 response: %s", body)
 	}
 
 	var data map[string]interface{}
 	if err := json.Unmarshal(body, &data); err != nil {
-		return Unknown, fmt.Errorf("client: error unmarshaling response: %v", err)
+		return Unknown, fmt.Errorf("http client error unmarshaling response: %v", err)
 	}
 
 	status, ok := data["status"].(string)
 	if !ok {
-		return Unknown, fmt.Errorf("client: unexpected response format: %s", body)
+		return Unknown, fmt.Errorf("http client unexpected response format: %s", body)
 	}
 
 	return HealthStatus(status), err
