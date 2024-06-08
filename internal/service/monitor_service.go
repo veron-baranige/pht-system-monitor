740601f3115ccb4713fe743f40492874644ce431
Veron Baranige <veron.b@hsenidoutsourcing.com>
2024-06-08 23:38:35 +0530

    Refactor monitor func into service

---
+++ internal/service/monitor_service.go
@@ -0,0 +1,81 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/martinlindhe/notify"
+	"github.com/veron-baranige/springboot-app-monitor/internal/config"
+	"github.com/veron-baranige/springboot-app-monitor/internal/monitor"
+	"github.com/veron-baranige/springboot-app-monitor/pkg/utils"
+)
+
+type MonitorConfig struct {
+    UrlsToMonitor 	 []string
+	MonitorInterval  time.Duration
+    AppLogoPath   	 string
+}
+
+type MonitorService struct {
+    config *MonitorConfig
+}
+
+func NewMonitorService(config MonitorConfig) *MonitorService {
+    return &MonitorService{
+        config: &config,
+    }
+}
+
+func (ms *MonitorService) Start() {
+	log.Println("monitoring service started")
+
+	ms.monitorHealthAndMetrics()
+
+	ticker := time.NewTicker(ms.config.MonitorInterval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		ms.monitorHealthAndMetrics()
+	}
+}
+
+func (ms *MonitorService) monitorHealthAndMetrics() {
+	if !utils.IsConnectedToInternet(config.ConnectivityTestUrl) {
+		msg := fmt.Sprintf("[%s] NO INTERNET CONNECTIVITY", time.Now().Format("15:04"))
+		notify.Notify("PHT System Monitor", "PHT System Monitor", msg, ms.config.AppLogoPath)
+		return
+	}
+
+	for i, baseUrl := range ms.config.UrlsToMonitor {
+		go func(baseUrl string) {
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+			defer cancel()
+
+			status, err := monitor.GetHealthStatus(ctx, baseUrl)
+			if err != nil {
+				notify.Alert(baseUrl, baseUrl, time.Now().Format("15:04")+" "+string(status), ms.config.AppLogoPath)
+			}
+
+			if status != monitor.Up {
+				notify.Alert(baseUrl, baseUrl, time.Now().Format("15:04")+" "+string(status), ms.config.AppLogoPath)
+			}
+
+			metrics, err := monitor.GetMetrics(ctx, baseUrl)
+			if err != nil {
+				return
+			}
+
+			content := fmt.Sprintf("[%v] CPU: %.2f%%, JVM: %.1f/%.1f GB, DISK: %.1f/%.1f GB",
+				time.Now().Format("15:04"), metrics.CpuUsage*metrics.CpuUsage, metrics.MemoryUsed, metrics.MemoryTotal,
+				metrics.DiskUsed, metrics.DiskTotal)
+			notify.Notify(baseUrl, baseUrl, content, ms.config.AppLogoPath)
+		}(baseUrl)
+
+		// wait before monitoring next app to provide notification read time
+		if len(ms.config.UrlsToMonitor) > 1 && i == len(ms.config.UrlsToMonitor)-1 {
+			time.Sleep(8 * time.Second)
+		}
+	}
+}
