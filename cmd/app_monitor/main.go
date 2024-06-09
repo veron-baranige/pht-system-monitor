fb74f0ad116eb10b5995077389c4fbd32799b572
Veron Baranige <veron.b@hsenidoutsourcing.com>
2024-06-09 12:10:28 +0530

    Add alert processing for desktop & emails

--- cmd/app_monitor/main.go
+++ cmd/app_monitor/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/viper"
 	"github.com/veron-baranige/springboot-app-monitor/internal/config"
 	"github.com/veron-baranige/springboot-app-monitor/internal/service"
+	"gopkg.in/gomail.v2"
 )
 
 func main() {
@@ -24,11 +25,24 @@ func main() {
 		log.Fatal(err)
 	}
 
+	mailDialer := gomail.NewDialer(
+		viper.GetString("SMTP_HOST"),
+		viper.GetInt("SMTP_PORT"),
+		viper.GetString("SMTP_USER"),
+		viper.GetString("SMTP_PASSWORD"),
+	)
+
 	monitorConf := &service.MonitorConfig{
-		UrlsToMonitor:       viper.GetStringSlice("SPRINGBOOT_APPLICATION_BASE_URLS"),
-		MonitorInterval:     time.Minute * viper.GetDuration("MONITOR_INTERVAL_MINUTES"),
-		AppLogoPath:         appLogoPath,
-		TestConnectivityUrl: config.ConnectivityTestUrl,
+		UrlsToMonitor:          viper.GetStringSlice("SPRINGBOOT_APPLICATION_BASE_URLS"),
+		MonitorInterval:        time.Minute * viper.GetDuration("MONITOR_INTERVAL_MINUTES"),
+		AppLogoPath:            appLogoPath,
+		TestConnectivityUrl:    config.ConnectivityTestUrl,
+		CpuUsageWarnThreshold:  viper.GetUint32("CPU_USAGE_WARN_THRESHOLD"),
+		JvmUsageWarnThreshold:  viper.GetUint32("JVM_MEMORY_USAGE_WARN_THRESHOLD"),
+		IsDesktopAlertsEnabled: viper.GetBool("ENABLE_DESKTOP_ALERTS"),
+		IsEmailAlertsEnabled:   viper.GetBool("ENABLE_EMAIL_ALERTS"),
+		MailDialer:             mailDialer,
+		EmailReceipients:       viper.GetStringSlice("EMAIL_ALERT_RECIPIENTS"),
 	}
 
 	monitorService := service.NewMonitorService(*monitorConf)
--- internal/service/monitor_service.go
+++ internal/service/monitor_service.go
@@ -2,34 +2,46 @@ package service
 
 import (
 	"context"
+	"crypto/tls"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/martinlindhe/notify"
+	"github.com/spf13/viper"
 	"github.com/veron-baranige/springboot-app-monitor/internal/monitor"
+	"gopkg.in/gomail.v2"
 )
 
 type MonitorConfig struct {
-	TestConnectivityUrl string
-	UrlsToMonitor       []string
-	MonitorInterval     time.Duration
-	AppLogoPath         string
+	TestConnectivityUrl   string
+	UrlsToMonitor         []string
+	MonitorInterval       time.Duration
+	AppLogoPath           string
+	CpuUsageWarnThreshold uint32
+	JvmUsageWarnThreshold uint32
+
+	MailDialer       *gomail.Dialer
+	EmailReceipients []string
+
+	IsDesktopAlertsEnabled bool
+	IsEmailAlertsEnabled   bool
 }
 
 type MonitorService struct {
-	config *MonitorConfig
+	config MonitorConfig
 }
 
 func NewMonitorService(config MonitorConfig) *MonitorService {
 	return &MonitorService{
-		config: &config,
+		config: config,
 	}
 }
 
 func (ms *MonitorService) Start() {
-	log.Println("monitoring service started")
+	log.Println("started monitoring service")
 
 	ms.monitorHealthAndMetrics()
 
@@ -43,8 +55,9 @@ func (ms *MonitorService) Start() {
 
 func (ms *MonitorService) monitorHealthAndMetrics() {
 	if !hasInternetConnection(ms.config.TestConnectivityUrl) {
-		msg := fmt.Sprintf("[%s] NO INTERNET CONNECTIVITY", time.Now().Format("15:04"))
-		notify.Notify("PHT System Monitor", "PHT System Monitor", msg, ms.config.AppLogoPath)
+		log.Println("No internect connection available for monitoring. Skipping monitoring for now.")
+		// msg := fmt.Sprintf("[%s] NO INTERNET CONNECTIVITY", time.Now().Format("15:04"))
+		// notify.Notify("Spring Boot App Monitor", "Spring Boot App Monitor", msg, ms.config.AppLogoPath)
 		return
 	}
 
@@ -55,30 +68,76 @@ func (ms *MonitorService) monitorHealthAndMetrics() {
 
 			status, err := monitor.GetHealthStatus(ctx, baseUrl)
 			if err != nil {
-				notify.Alert(baseUrl, baseUrl, time.Now().Format("15:04")+" "+string(status), ms.config.AppLogoPath)
+				if errors.Is(err, monitor.ErrNoActuatorSupport) {
+					msg := fmt.Sprintf("[%s] No actuator support for: %s", time.Now().Format("15:04"), baseUrl+"/actuator")
+					ms.handleAlert(baseUrl, msg, true, true)
+					return
+				}
+
+				if errors.Is(err, monitor.ErrNotResponding) {
+					msg := fmt.Sprintf("[%s] %s", time.Now().Format("15:04"), "No response from app. Attention required!")
+					ms.handleAlert(baseUrl, msg, true, true)
+					return
+				}
+
+				msg := fmt.Sprintf("[%s] Failed to get health status: %s", time.Now().Format("15:04"), err)
+				ms.handleAlert(baseUrl, msg, true, true)
+				return
 			}
 
 			if status != monitor.Up {
-				notify.Alert(baseUrl, baseUrl, time.Now().Format("15:04")+" "+string(status), ms.config.AppLogoPath)
+				msg := fmt.Sprintf("[%s] Health Status: %s. Attention required!", time.Now().Format("15:04"), string(status))
+				ms.handleAlert(baseUrl, msg, true, true)
+				return
 			}
 
 			metrics, err := monitor.GetMetrics(ctx, baseUrl)
 			if err != nil {
+				msg := fmt.Sprintf("[%s] Health status: %s. Failed to get metrics: %s", time.Now().Format("15:04"), string(status), err)
+				ms.handleAlert(baseUrl, msg, false, false)
 				return
 			}
 
-			content := fmt.Sprintf("[%v] CPU: %.2f%%, JVM: %.1f/%.1f GB, DISK: %.1f/%.1f GB",
+			msg := fmt.Sprintf("[%v] CPU: %.2f%%, JVM: %.1f/%.1f GB, DISK: %.1f/%.1f GB",
 				time.Now().Format("15:04"), metrics.CpuUsage*metrics.CpuUsage, metrics.MemoryUsed, metrics.MemoryTotal,
 				metrics.DiskUsed, metrics.DiskTotal)
-			notify.Notify(baseUrl, baseUrl, content, ms.config.AppLogoPath)
+			ms.handleAlert(baseUrl, msg, false, false)
 		}(baseUrl)
 
 		// wait before monitoring next app to provide notification read time
 		if len(ms.config.UrlsToMonitor) > 1 && i == len(ms.config.UrlsToMonitor)-1 {
-			time.Sleep(8 * time.Second)
+			time.Sleep(6 * time.Second)
 		}
 	}
 }
+
+func (ms *MonitorService) handleAlert(appBaseUrl string, msgContent string, isAlert bool, sendMail bool) {
+	if ms.config.IsDesktopAlertsEnabled {
+		if isAlert {
+			notify.Alert(appBaseUrl, appBaseUrl, msgContent, ms.config.AppLogoPath)
+		} else {
+			notify.Notify(appBaseUrl, appBaseUrl, msgContent, ms.config.AppLogoPath)
+		}
+	}
+	if ms.config.IsEmailAlertsEnabled && sendMail {
+		mailErr := sendEmail(ms.config.MailDialer, ms.config.EmailReceipients, "Spring Boot App Monitor - " + appBaseUrl, msgContent)
+		if mailErr != nil {
+			log.Println("Failed to send email: ", mailErr)
+		}
+	}
+}
+
+func sendEmail(mailDialer *gomail.Dialer, recipients []string, subject string, body string) error {
+	message := gomail.NewMessage()
+
+	message.SetHeader("From", viper.GetString("SMTP_USER"))
+	message.SetHeader("To", recipients...)
+	message.SetHeader("Subject", subject)
+	message.SetBody("text/plain", body)
+
+	mailDialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	return mailDialer.DialAndSend(message)
+}	
 
 func hasInternetConnection(testConnUrl string) bool {
 	_, err := http.Get(testConnUrl)
